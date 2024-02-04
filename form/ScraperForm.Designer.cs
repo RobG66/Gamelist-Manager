3ec1d386107914e78522da54cbc33377a792cbc9
RobG66 <silverball.slayer@gmail.com>
2024-02-03 19:57:14 -0600

    3.1 Work

--- form/ScraperForm.Designer.cs
+++ form/ScraperForm.Designer.cs
@@ -241,8 +241,7 @@
             this.comboBox_Scrapers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox_Scrapers.FormattingEnabled = true;
             this.comboBox_Scrapers.Items.AddRange(new object[] {
-            "ArcadeDB",
-            "ScreenScraper"});
+            "ArcadeDB"});
             this.comboBox_Scrapers.Location = new System.Drawing.Point(59, 70);
             this.comboBox_Scrapers.Margin = new System.Windows.Forms.Padding(1);
             this.comboBox_Scrapers.Name = "comboBox_Scrapers";
